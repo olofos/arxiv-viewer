d4291bcde115e1849a817c91f052d704dc5442ef
Olof Ohlsson Sax <olof.ohlsson.sax@gmail.com>
2022-11-13 15:52:21 +0100

    Fix eslint warnings

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
-import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import MainTabNavigator from './src/navigation/MainTabNavigator';
 
 import Colors from './src/constants/Colors';
@@ -17,17 +16,9 @@ const Theme = {
 export default function App() {
   return (
     <NavigationContainer theme={Theme}>
-      <StatusBar style='light' />
+      {/* eslint-disable-next-line react/style-prop-object */}
+      <StatusBar style="light" />
       <MainTabNavigator />
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

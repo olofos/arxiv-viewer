5d72c62cc28bf66d9515a04609d29ddd69557279
Olof Ohlsson Sax <olof.ohlsson.sax@gmail.com>
2022-11-23 12:04:09 +0100

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,6 @@ module.exports = {
         'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
         'react/prop-types': 'off',
         'no-use-before-define': 'off',
+        'no-console': 'off',
     },
 };
